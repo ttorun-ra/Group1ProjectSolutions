fae5a0f4796fed10287d446c955665e86b1eee88
yvzydn <yavuzaydinus@gmail.com>
2019-11-26 10:39:00 -0500

    Group 1's Last project solutions

--- src/MethodsCode.java
+++ src/MethodsCode.java
@@ -13,15 +13,47 @@ public class MethodsCode {
      * if digit is even you digit is multiplied by 2
      * if digit is odd you digit is multiplied by -1
      * Note: if sum is zero then return -1
-     *
+     * <p>
      * ex1: you passed argument "85" and return is 11
      * because: (8 * 2) + (5 * -1) = 11
-     *
+     * <p>
      * ex2: "0" ->  -1
      * because:
      */
     //write you code here
+    public static int getTotal(String number) {
+//        int number = Integer.parseInt(a);
+//        if (number == 0) {
+//            return -1;
+//        }
+//        int sum = 0;
+//        int remainder = 0;
+//        while (number != 0) {
+//            remainder = number / 10;
+//            if (remainder % 2 == 0) {
+//                sum = sum + remainder * 2;
+//                number = number / 10;
+//            } else {
+//                sum=sum+ remainder * -1;
+//                number = number / 10;
+//            }
+//        }
+//
+//    return sum ;
+//}
+int sum=0;
+for (int i = 0; i < number.length(); i++) {
 
+            Integer digit = Integer.valueOf(number.substring(i, i + 1));
+            if (digit % 2 == 0) {
+                sum += digit * 2;
+            } else {
+                sum += digit * -1;
+            }
+        }return sum == 0 ? -1 : sum;
+
+
+    }
     //end
 
     /**
@@ -36,10 +68,20 @@ public class MethodsCode {
      * Hint: remove the unnecessary icons and calculate the total
      */
     //write you code here
-
-    //end
-
-
+    public double bankStatementCalculator (List<String> bankStatements) {
+        double sum = 0;
+        for (String str: bankStatements) {
+            String replace = str.replace("$", "");
+            String replace2 = replace.replace(",", "");
+            double num = Double.valueOf(replace2);
+            sum += num;
+        }
+        if (sum == 0) {
+            return -1;
+        }
+        else
+            return sum;
+    }
     /**
      * TASK 3
      * Write a method named randomGenerator
@@ -54,6 +96,20 @@ public class MethodsCode {
      */
     //write you code here
 
+    public int randomGenerator(int bound) {
+        if (bound < 1) {
+            return -1;
+        }
+        if (bound == 1) {
+            return 1;
+        }else{
+
+        }
+        Random random = new Random();
+        int rand = random.nextInt(bound);
+        return bound;
+
+    }
     //end
 
 
@@ -85,6 +141,16 @@ public class MethodsCode {
      * return is "-1"
      */
     //write you code here
+    public String getPopulation (List<String> countries, List<String> populations, String randomCountry) {
+        if (countries.contains(randomCountry)) {
+            int index = countries.indexOf(randomCountry);
+            return populations.get(index);
+        }
+        else
+            return "-1";
+    }
+
+
 
     //end
 
@@ -105,7 +171,18 @@ public class MethodsCode {
      * return {"Alice", "Bob", "Ali"}
      */
     // write your code here
+        public List<String> correctListOfNames (List<String> names, String wrongName, String correctName){
 
-    //end
+            for (int i=0;i<names.size();i++) {
 
-}
+                if (names.get(i).equals(wrongName)){
+
+                    names.set(i,correctName);
+                }
+                }
+                  return names;
+                }
+            }
+   //end
+
+
